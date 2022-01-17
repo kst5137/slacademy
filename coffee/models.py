4ed9e5bf16bb15e8cd16fc8badf06e1ad88dd7b5
kst5137 <67016829+kst5137@users.noreply.github.com>
2022-01-17 15:55:01 +0900

    Delete coffee directory

--- coffee/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Comment(models.Model):
-    title = models.CharField(max_length=200)
-    contents = models.TextField()
-    create_date = models.DateTimeField(auto_now_add=True)
-
